b64af1b749ebde194d905e0483f3c9960abdcd21
Iryna Ohurtsova <og.irkaa@gmail.com>
2022-01-30 21:05:38 +0200

    add logger

--- src/main/java/org/bus/ticket/management/exceptionhandler/DefaultExceptionHandler.java
+++ src/main/java/org/bus/ticket/management/exceptionhandler/DefaultExceptionHandler.java
@@ -1,24 +1,35 @@
 package org.bus.ticket.management.exceptionhandler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bus.ticket.management.dto.ErrorMessageDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class DefaultExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorMessageDto handleException(IllegalArgumentException exception) {
+        log.error(exception.getMessage(), exception);
         return new ErrorMessageDto(exception.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessageDto handleException(IllegalStateException exception) {
+        log.error(exception.getMessage(), exception);
         return new ErrorMessageDto(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ResponseEntity.internalServerError().build();
     }
 }
